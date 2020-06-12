dbaac208807e17c282ee9682e1c300fc783e6da3
seanmx96 <seanmx96@users.noreply.github.com>
2020-06-11 23:55:28 -0500

    Created Zoo Controller

--- javazoos/src/main/java/com/lambdaschool/javazoos/controllers/ZooController.java
+++ javazoos/src/main/java/com/lambdaschool/javazoos/controllers/ZooController.java
@@ -3,13 +3,16 @@ package com.lambdaschool.javazoos.controllers;
 import com.lambdaschool.javazoos.models.Zoo;
 import com.lambdaschool.javazoos.services.ZooService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -34,5 +37,64 @@ public class ZooController
         return new ResponseEntity<>(z, HttpStatus.OK);
     }
 
+    @PostMapping(value = "/zoo",
+        consumes = {"application/json"})
+    public ResponseEntity<?> addNewZoo(
+        @Valid
+        @RequestBody
+            Zoo newzoo) throws URISyntaxException
+    {
+        newzoo.setZooid(0);
+        newzoo = zooService.save(newzoo);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{zooid}")
+            .buildAndExpand(newzoo.getZooid())
+            .toUri();
+        responseHeaders.setLocation(newUserURI);
+
+        return new ResponseEntity<>(null,
+            responseHeaders,
+            HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/zoo/{zooid}",
+        consumes = {"application/json"})
+    public ResponseEntity<?> updateFullZoo(
+        @Valid
+        @RequestBody
+            Zoo updateZoo,
+        @PathVariable
+            long zooid)
+    {
+        updateZoo.setZooid(zooid);
+        zooService.save(updateZoo);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/zoo/{zooid}",
+        consumes = {"application/json"})
+    public ResponseEntity<?> updateZoo(
+        @RequestBody
+            Zoo updateZoo,
+        @PathVariable
+            long id)
+    {
+        zooService.update(updateZoo,
+            id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/zoo/{zooid}")
+    public ResponseEntity<?> deleteZooById(
+        @PathVariable
+            long id)
+    {
+        zooService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
